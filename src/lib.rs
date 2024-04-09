a15f9679f0aac04e05fe4150a461aa4698d67172
Wouter Doppenberg <wouterdoppenberg@gmail.com>
2024-04-09 15:26:08 +0200

    Updated readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,4 @@
-//! # `glowrs`
-//! 
-//! `glowrs` provides an easy and familiar interface to use pre-trained models for embeddings and sentence similarity.
-//! 
-//! ## Example
-//! 
-//! ```rust
-//! use glowrs::SentenceTransformer;
-//! 
-//! let encoder = SentenceTransformer::from_repo_string("sentence-transformers/all-MiniLM-L6-v2").unwrap();
-//! 
-//! let sentences = vec![
-//!    "Hello, how are you?",
-//!    "Hey, how are you doing?"
-//! ];
-//! 
-//! let embeddings = encoder.encode_batch(sentences, true).unwrap();
-//! 
-//! println!("{:?}", embeddings);
-//! ```
-//! 
-//! ## Features
-//! 
-//! - Load models from Hugging Face Hub
-//! - Spin up a REST API server to serve embeddings
-//! - More to come!
-//! 
-//! ## Running the server
+
  
 #![doc = include_str!("../README.md")]
 
