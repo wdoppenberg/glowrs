328fd1f4e531ae9a26416606cfde088f16fc398c
Wouter Doppenberg <wouterdoppenberg@gmail.com>
2024-04-16 09:59:56 +0200

    Added distilbert support (#24)
    * Added distilbert support
    
    * bumped version
    
    * license-file

--- crates/glowrs/src/model/embedder.rs
+++ crates/glowrs/src/model/embedder.rs
@@ -2,14 +2,17 @@ use anyhow::{Context, Error, Result};
 use candle_core::{DType, Module, Tensor};
 use candle_nn::VarBuilder;
 use candle_transformers::models::{
-    bert::Config as BertConfig, jina_bert::Config as JinaBertConfig,
+    bert::Config as BertConfig, distilbert::Config as DistilBertConfig,
+    jina_bert::Config as JinaBertConfig,
 };
 use std::ops::Deref;
 use std::path::Path;
 use tokenizers::Tokenizer;
 
 // Re-exports
-pub use candle_transformers::models::{bert::BertModel, jina_bert::BertModel as JinaBertModel};
+pub use candle_transformers::models::{
+    bert::BertModel, distilbert::DistilBertModel, jina_bert::BertModel as JinaBertModel,
+};
 use serde::Deserialize;
 
 use crate::model::device::DEVICE;
@@ -22,6 +25,7 @@ use candle_nn::VarMap;
 pub(crate) enum ModelConfig {
     Bert(BertConfig),
     JinaBert(JinaBertConfig),
+    DistilBert(DistilBertConfig),
 }
 
 #[derive(Deserialize)]
@@ -51,6 +55,10 @@ pub(crate) fn parse_config(config_str: &str) -> Result<ModelConfig> {
                     let config: JinaBertConfig = serde_json::from_str(config_str)?;
                     ModelConfig::JinaBert(config)
                 }
+                Some("DistilBertForMaskedLM") => {
+                    let config: DistilBertConfig = serde_json::from_str(config_str)?;
+                    ModelConfig::DistilBert(config)
+                }
                 _ => return Err(Error::msg("Invalid model architecture")),
             }
         }
@@ -67,6 +75,7 @@ where
     match model_config {
         ModelConfig::Bert(cfg) => Ok(T::from(Box::new(BertModel::load(vb, &cfg)?))),
         ModelConfig::JinaBert(cfg) => Ok(T::from(Box::new(JinaBertModel::new(vb, &cfg)?))),
+        ModelConfig::DistilBert(cfg) => Ok(T::from(Box::new(DistilBertModel::load(vb, &cfg)?))),
     }
 }
 
@@ -100,6 +109,14 @@ impl EmbedderModel for JinaBertModel {
     #[inline]
     fn encode(&self, token_ids: &Tensor) -> Result<Tensor> {
         Ok(self.forward(token_ids)?)
+    }
+}
+
+impl EmbedderModel for DistilBertModel {
+    #[inline]
+    fn encode(&self, token_ids: &Tensor) -> Result<Tensor> {
+        let attention_mask = token_ids.ones_like()?;
+        Ok(self.forward(token_ids, &attention_mask)?)
     }
 }
 
@@ -212,6 +229,7 @@ mod test {
 
     const BERT_CONFIG_PATH: &str = "tests/fixtures/all-MiniLM-L6-v2/config.json";
     const JINABERT_CONFIG_PATH: &str = "tests/fixtures/jina-embeddings-v2-base-en/config.json";
+    const DISTILBERT_CONFIG_PATH: &str = "tests/fixtures/multi-qa-distilbert-dot-v1/config.json";
 
     #[test]
     fn test_parse_config_bert() -> Result<()> {
@@ -245,47 +263,23 @@ mod test {
     }
 
     #[test]
-    fn test_zeros_model_bert() -> Result<()> {
-        let path = Path::new(BERT_CONFIG_PATH);
+    fn test_parse_config_distilbert() -> Result<()> {
+        let path = Path::new(DISTILBERT_CONFIG_PATH);
 
         let config_str = std::fs::read_to_string(path)?;
+
         let config = parse_config(&config_str)?;
 
-        let model: Box<dyn EmbedderModel> = load_zeros_model(config)?;
-
-        let token_ids = Tensor::zeros(&[1, 128], DType::U32, &DEVICE)?;
-
-        let embeddings = model.encode(&token_ids)?;
-
-        let (_n_sentence, out_tokens, _hidden_size) = embeddings.dims3()?;
-
-        assert_eq!(out_tokens, 128);
+        match config {
+            ModelConfig::DistilBert(_) => {}
+            _ => panic!("Invalid config type"),
+        }
 
         Ok(())
     }
 
     #[test]
-    fn test_zeros_model_jinabert() -> Result<()> {
-        let path = Path::new(JINABERT_CONFIG_PATH);
-
-        let config_str = std::fs::read_to_string(path)?;
-        let config = parse_config(&config_str)?;
-
-        let model: Box<dyn EmbedderModel> = load_zeros_model(config)?;
-
-        let token_ids = Tensor::zeros(&[1, 128], DType::U32, &DEVICE)?;
-
-        let embeddings = model.encode(&token_ids)?;
-
-        let (_n_sentence, out_tokens, _hidden_size) = embeddings.dims3()?;
-
-        assert_eq!(out_tokens, 128);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_random_model_bert() -> Result<()> {
+    fn test_forward_bert() -> Result<()> {
         let path = Path::new(BERT_CONFIG_PATH);
 
         let config_str = std::fs::read_to_string(path)?;
@@ -305,8 +299,28 @@ mod test {
     }
 
     #[test]
-    fn test_random_model_jinabert() -> Result<()> {
+    fn test_forward_jinabert() -> Result<()> {
         let path = Path::new(JINABERT_CONFIG_PATH);
+
+        let config_str = std::fs::read_to_string(path)?;
+        let config = parse_config(&config_str)?;
+
+        let model: Box<dyn EmbedderModel> = load_random_model(config)?;
+
+        let token_ids = Tensor::zeros(&[1, 128], DType::U32, &DEVICE)?;
+
+        let embeddings = model.encode(&token_ids)?;
+
+        let (_n_sentence, out_tokens, _hidden_size) = embeddings.dims3()?;
+
+        assert_eq!(out_tokens, 128);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_forward_distilbert() -> Result<()> {
+        let path = Path::new(DISTILBERT_CONFIG_PATH);
 
         let config_str = std::fs::read_to_string(path)?;
         let config = parse_config(&config_str)?;
